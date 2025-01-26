e5441a639a4809e32270589f59b03bff19c7acbc
DominiceLanzarini <dlanzarini@gmail.com>
2025-01-26 16:48:47 -0300

    Ex 1 - Estrutura Inicial

---
+++ src/database/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata"
+import { DataSource } from "typeorm"
+
+
+export const AppDataSource = new DataSource({
+    type: "postgres",
+    host: "localhost",
+    port: 5433,
+    username: "postgres",
+    password: "admin",
+    database: "projeto_vitrine_medicamentos",
+    synchronize: true,
+    logging: true,
+    entities: [],
+    migrations: ["src/database/migrations/*.ts"]
+})
